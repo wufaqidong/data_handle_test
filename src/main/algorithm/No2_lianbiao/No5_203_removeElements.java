88e46e43f9b7eb6b49df4b5f22bb59cddc1ba072
xbinbin <xb8023#XB>
2022-05-26 23:04:46 +0800

    算法-链表-移除链表元素

--- src/main/algorithm/No2_lianbiao/No5_203_removeElements.java
+++ src/main/algorithm/No2_lianbiao/No5_203_removeElements.java
@@ -5,75 +5,68 @@
  * @create: 2022-05-26 22:12
  **/
 
-// 题目链接：https://leetcode-cn.com/problems/spiral-matrix-ii/
-// 给定一个正整数n，生成一个包含 1 到n^2所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵。
-// [螺旋矩阵](https://img-blog.csdnimg.cn/2020121623550681.png)
-//    示例:
+// 题目链接：https://leetcode-cn.com/problems/remove-linked-list-elements/
+// 删除链表中等于给定值 val 的所有节点。
+//示例 1：
+//输入：head = [1,2,6,3,4,5,6], val = 6
+//输出：[1,2,3,4,5]
 //
-//            输入: 3
-//            输出:
-//            [
-//            [ 1, 2, 3 ],
-//            [ 8, 9, 4 ],
-//            [ 7, 6, 5 ]
-//            ]
-public class No2_27_removeNum {
-    // 第一种暴力解法：两个for循环，然后不断的寻找符合条件的子序列。
-    // 很明显暴力解法的时间复杂度是O(n^2)
-    public int[][] generateMatrix(int n) {
-        int[][] res = new int[n][n];
+//示例 2：
+//输入：head = [], val = 1
+//输出：[]
+//
+//示例 3：
+//输入：head = [7,7,7,7], val = 7
+//输出：[]
 
-        // 循环次数
-        int loop = n / 2;
-
-        // 定义每次循环起始位置
-        int startX = 0;
-        int startY = 0;
-
-        // 定义偏移量
-        int offset = 1;
-
-        // 定义填充数字
-        int count = 1;
-
-        // 定义中间位置
-        int mid = n / 2;
-        while (loop > 0) {
-            int i = startX;
-            int j = startY;
-
-            // 模拟上侧从左到右
-            for (; j<startY + n -offset; ++j) {
-                res[startX][j] = count++;
-            }
-
-            // 模拟右侧从上到下
-            for (; i<startX + n -offset; ++i) {
-                res[i][j] = count++;
-            }
-
-            // 模拟下侧从右到左
-            for (; j > startY; j--) {
-                res[i][j] = count++;
-            }
-
-            // 模拟左侧从下到上
-            for (; i > startX; i--) {
-                res[i][j] = count++;
-            }
-
-            loop--;
-
-            startX += 1;
-            startY += 1;
-
-            offset += 2;
+// 这里就涉及如下链表操作的两种方式：
+// 方法一：直接使用原来的链表来进行删除操作。
+// 方法二：设置一个虚拟头结点在进行删除操作。
+public ListNode removeElements(ListNode head,int val){
+        if(head==null){
+        return head;
         }
-
-        if (n % 2 == 1) {
-            res[mid][mid] = count;
+        // 因为删除可能涉及到头节点，所以设置dummy节点，统一操作
+        ListNode dummy=new ListNode(-1,head);
+        ListNode pre=dummy;
+        ListNode cur=head;
+        while(cur!=null){
+        if(cur.val==val){
+        pre.next=cur.next;
+        }else{
+        pre=cur;
         }
-
-        return res;
-    }
-}
+        cur=cur.next;
+        }
+        return dummy.next;
+        }
+/**
+ * 不添加虚拟节点方式
+ * 时间复杂度 O(n)
+ * 空间复杂度 O(1)
+ *
+ * @param head
+ * @param val
+ * @return
+ */
+public ListNode removeElements(ListNode head,int val){
+        while(head!=null&&head.val==val){
+        head=head.next;
+        }
+        // 已经为null，提前退出
+        if(head==null){
+        return head;
+        }
+        // 已确定当前head.val != val
+        ListNode pre=head;
+        ListNode cur=head.next;
+        while(cur!=null){
+        if(cur.val==val){
+        pre.next=cur.next;
+        }else{
+        pre=cur;
+        }
+        cur=cur.next;
+        }
+        return head;
+        }
